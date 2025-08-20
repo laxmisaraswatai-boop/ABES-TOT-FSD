8b1d92cfdcdd42d52643d38ae2684dfe1466be07
laxmi <laxmi.saraswat@abes.ac.in>
2025-08-20 11:43:00 +0530

    JS

---
+++ JS work/a.js
@@ -0,0 +1,5 @@
+var a=9;
+console.log("hi"+a);
+
+
+
---
+++ JS/index.js
@@ -0,0 +1,60 @@
+// var a=12;
+// console.log("hi="+a);
+// var a=30;
+// console.log(typeof(a));
+// var a=12;
+// var b=20;
+// var c=a+b;
+// 
+// let a = 12;
+// let b = "12";
+// if(a==b){
+//     console.log("hi")
+// }
+// else {
+//     console.log("hi..JS")
+// }
+// function sum(a,b){
+//     return a+b;
+// }
+// let val=sum(12,20)
+// console.log("sum="+val);
+// //function expression
+//arrow 
+// const data = () => {
+//     return "Hello World";
+// }
+// console.log(data());
+// const sum = (a,b) => {
+//     return a+b;
+// }
+// const add = (a,b) => a+b;
+// add(3,4);
+//IIFE
+// function greet(msg="hello"){
+//     return "hi="+msg;
+// }
+// console.log(greet("welcome"));
+function selectlang(lang="java"){
+    let data;
+    console.log("Selected language="+lang);
+    if(lang=="java"){
+        function javacompiler(){
+            console.log("javacompiler")
+        }
+        data = javacompiler();
+        console.log("Compiler output="+data);
+    }
+}
+// else if(lang=="c"){
+//       function ccompiler(){
+// //          console.log("ccompiler")
+// //         }
+// //         data = ccompiler();
+// //     }
+// //     else{
+// //         function pythoncompiler(){
+// //             console.log("pythoncompiler")
+// //         }
+// //     data = pythoncompiler();
+// // }
